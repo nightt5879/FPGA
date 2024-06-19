8907aa34d47ef1b112747bae74be45a15a59fccb
Nightt <87569709+nightt5879@users.noreply.github.com>
2024-06-19 15:05:40 +0800

    start the project

---
+++ led/led.srcs/sources_1/new/led.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/06/19 15:01:37
+// Design Name: 
+// Module Name: led
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module led(
+
+    );
+endmodule
