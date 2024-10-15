640ce97eab59b60cdc8abf29cb47199ed646fc5e
Nightt <87569709+nightt5879@users.noreply.github.com>
2024-10-15 20:43:42 +0800

    delete all the file
    something wrong  & i think i has fixed it, so just try again

--- ZedBoard/HLS_lab/lab1/matrixmul/matrixmul/hls/.autopilot/db/kernel_module_hierarchy.tcl
+++
@@ -1,5 +0,0 @@
-set ModuleHierarchy {[{
-"Name" : "matrixmul","ID" : "0","Type" : "pipeline",
-"SubLoops" : [
-	{"Name" : "Row_Col","ID" : "1","Type" : "pipeline"},]
-}]}
--- ZedBoard/HLS_lab/lab1/matrixmul/matrixmul/hls/csim/build/sim.bat
+++
@@ -1,1 +0,0 @@
-    E:/vivado/Vitis_HLS/2024.1/bin/vitis_hls run_sim.tcl
